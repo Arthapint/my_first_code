a1686733c792bdcdf51c5473250322bb8a5c1c87
Arthapint <149014708+Arthapint@users.noreply.github.com>
2023-10-25 21:20:16 +0100

    Add files via upload

---
+++ my_first_code.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This is my first line of code.
+
+
+# In[2]:
+
+
+print("My name is Arthur Whitley")
+print("The date is 25/10/2023")
+
+
+# In[3]:
+
+
+# Write a program to analyse real estate data.
+
+
+# In[4]:
+
+
+#A is property ID and corresponding number is its sales price value
+a= 45000
+b= 23400
+c= 67000
+d= 34600
+e= 12900
+
+
+# In[6]:
+
+
+print(a,b,c,d,e)
+
+
+# In[9]:
+
+
+#Total sales from december
+f=a+b+c+d+e
+print(f)
+
+
+# In[15]:
+
+
+#average sales price for december
+f / 5
+
+
+# In[16]:
+
+
+# property price for property c
+c - 6500
+
+
+# In[ ]:
+
+
+
+
